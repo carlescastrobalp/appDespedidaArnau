cca6ecf61ca2a00549c890f9e40cc14f601a838b
carlescastrobalp <carles.albatarrec91@gmail.com>
2025-04-11 16:58:43 +0200

    Se sube el apk version random

--- app/src/main/java/com/carlescastro/despedidaarnau/DetallesObjeto.java
+++ app/src/main/java/com/carlescastro/despedidaarnau/DetallesObjeto.java
@@ -1,7 +1,5 @@
 package com.carlescastro.despedidaarnau;
 
-import static android.view.View.INVISIBLE;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,11 +10,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Objects;
 
 public class DetallesObjeto extends AppCompatActivity {
     private TextView textPreguntaOPrueba, textNivel, textDescripcion;
