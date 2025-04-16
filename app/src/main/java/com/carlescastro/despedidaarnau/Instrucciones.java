5f8d3df298e625f48358d9f4e9920b73aeebcb17
carlescastrobalp <carles.albatarrec91@gmail.com>
2025-04-16 21:38:18 +0200

    Es puja la layout main i part de la d'instruccions

--- app/src/main/java/com/carlescastro/despedidaarnau/Instrucciones.java
+++ app/src/main/java/com/carlescastro/despedidaarnau/Instrucciones.java
@@ -1,9 +1,12 @@
 package com.carlescastro.despedidaarnau;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +15,13 @@ public class Instrucciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones); // Vincula con instructions.xml
-
         Button btnInsert = findViewById(R.id.btn_insert);
+
+        TextView instruccionesTextView = findViewById(R.id.pantalla2);
+        String instrucciones = getString(R.string.instruccions);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            instruccionesTextView.setText(Html.fromHtml(instrucciones, Html.FROM_HTML_MODE_LEGACY));
+        }
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/carlescastro/despedidaarnau/MainActivity.java
+++ app/src/main/java/com/carlescastro/despedidaarnau/MainActivity.java
@@ -1,10 +1,14 @@
 package com.carlescastro.despedidaarnau;
 
+import static android.view.View.INVISIBLE;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,15 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnMain = findViewById(R.id.btn_instrucciones);
+        ImageView miImagen = findViewById(R.id.imatgePrincipal);
+        View fondoPrincipal = findViewById(R.id.fondoPrincipal);
+
 
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentLayoutInstruccion = new Intent(MainActivity.this, Instrucciones.class);
-                startActivity(intentLayoutInstruccion);
-                finish();
+
+                miImagen.setVisibility(INVISIBLE);
+                btnMain.setEnabled(false);
+                btnMain.setVisibility(INVISIBLE);
+
+                // Cambia el fondo del layout principal
+                fondoPrincipal.setBackgroundResource(R.drawable.button_ivan); // tu imagen en drawable
+
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run() {
+                        Intent intentLayoutInstruccion = new Intent(MainActivity.this, Instrucciones.class);
+                        startActivity(intentLayoutInstruccion);
+                        finish();
+                    }
+                }, 500);
             }
         });
-
     }
 }
